79e295d2ae0d5aca23e433856c71a3d324d9dd7b
Eric Couch <ericcouch@yahoo.com>
2022-11-21 11:28:54 -0600

    Add project files.

---
+++ ClassExample/Card.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassExample
+{
+    internal class Card
+    {
+        public string suit { get; set; }
+        public string rank { get; set; }
+        public int val { get; set; }
+
+        /// <summary>
+        /// This is a card class
+        /// </summary>
+        /// <param name="rank">A, K, Q, J, 10, 9...2</param>
+        /// <param name="suit">spades, hearts, clubs, diamonds (use ASCII code)</param>
+        public Card(string rank, string suit, int val)
+        {
+            this.suit = suit;
+            this.rank = rank;
+            this.val = val;
+        }
+
+        public override string? ToString()
+        {
+            return String.Format($"{rank}{suit}");
+        }
+    }
+}
---
+++ ClassExample/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace ClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Using the Card class that we created earlier, 
+             * create a program that deals a player 5 cards.  
+             * With time permitting, implement the ability 
+             * for a user to discard up to 4 cards if they 
+             * have an Ace, or 3 if they do not.  Then deal 
+             * the player new cards for every card discarded.
+             */
+            List<Card> Deck = new List<Card>();
+
+            for (int rank = 2; rank <= 14; rank++)
+            {
+                for (int suit = 0; suit < 4; suit++)
+                {
+                    string convertedRank = String.Empty;
+                    string convertedSuit = String.Empty;
+
+                    switch (rank)
+                    {
+                        case 11:
+                            convertedRank = "J";
+                            break;
+                        case 12:
+                            convertedRank = "Q";
+                            break;
+                        case 13:
+                            convertedRank = "K";
+                            break;
+                        case 14:
+                            convertedRank = "A";
+                            break;
+                        default:
+                            convertedRank = rank.ToString();
+                            break;
+                    }
+                    switch (suit)
+                    {
+                        case 0:
+                            convertedSuit = "♠";
+                            break;
+                        case 1:
+                            convertedSuit = "♣";
+                            break;
+                        case 2:
+                            convertedSuit = "♥";
+                            break;
+                        case 3:
+                            convertedSuit = "♦";
+                            break;
+                    }
+
+                    Card currentCard = new Card(convertedRank, convertedSuit, rank);
+                    Deck.Add(currentCard);
+                }
+            }
+            //foreach (Card thisCard in Deck)
+            //{
+            //    Console.WriteLine($"Rank: {thisCard.rank} Suit: {thisCard.suit}");
+            //}
+            Console.WriteLine("Here are your cards:");
+            for (int cardNum = 0; cardNum < 5; cardNum++)
+            {
+                Card thisCard = DealCard(Deck);
+                Console.Write($"{thisCard.ToString()}\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"How many cards left in our deck: {Deck.Count}");
+        }
+
+        public static Card DealCard(List<Card> cards)
+        {
+            Random rnd = new Random();
+            int cardToSelect = rnd.Next(cards.Count);
+            Card dealtCard = cards[cardToSelect];
+            cards.RemoveAt(cardToSelect);
+            return dealtCard;
+        }
+    }
+}
